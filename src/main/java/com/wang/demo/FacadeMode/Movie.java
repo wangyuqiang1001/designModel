65220945835a729df88907f7129c0ea179c6abb9
wangyuqiang1001 <1306237132@qq.com>
2020-03-05 10:41:44 +0800

    外观模式简要描述

---
+++ src/main/java/com/wang/demo/FacadeMode/Movie.java
@@ -0,0 +1,13 @@
+package com.wang.demo.FacadeMode;
+
+public class Movie {
+
+
+    /**
+     * 简而言之，外观模式就是提供一个高级接口，将需要做的一系列工作集成:
+     *  参考原博地址: https://blog.csdn.net/lmj623565791/article/details/25837275
+     */
+    public void watchMovie() {
+
+    }
+}
